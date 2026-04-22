f19dda01406041b6d08b26bc7ca9043bad2941a4
shahilaqeel90 <shahil.25b15410307@abes.ac.in>
2026-04-22 14:21:27 +0530

    exp 7

--- react/experiment 7/src/main.jsx
+++ react/experiment 7/src/main.jsx
@@ -6,5 +6,14 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+)// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
